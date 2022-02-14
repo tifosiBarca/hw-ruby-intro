02e89126c494f29003eea66935088b6accb950c1
EC2 Default User <ec2-user@ip-172-31-11-98.ec2.internal>
2022-02-14 03:56:05 +0000

    commit

--- hello.rb
+++
@@ -1,9 +0,0 @@
-puts "Hello, World!"
-
-puts "The sum of 1 and 3 is 4."
-
-argv0 = ARGV[0]
-argv1 = ARGV[1]
-sum = argv0.to_i + argv1.to_i
-
-puts "The sum of #{argv0} and #{argv1} is #{sum}."
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,7 +2,7 @@
 
 # Part 1
 
-#print("hello ruby")
+print("hello ruby")
 
 def sum arr
   # YOUR CODE HERE
